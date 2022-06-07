2f665bd32d180111188cf5b729097ee56631e048
sanskar5686 <88050679+sanskar5686@users.noreply.github.com>
2022-06-07 23:26:41 +0530

    Update DAY-1.cpp

--- DAY-1.cpp
+++ DAY-1.cpp
@@ -1,1 +1,20 @@
 //  Welcome to day-1
+vector<vector<long long int>> printPascal(int n) 
+{
+  // Write your code here.
+    vector<vector<long long int>> ans;
+    vector<long long int> temp;
+    for(int i=0;i<n;i++){
+        temp.clear();
+        for(int j=0;j<=i;j++){
+            if(i==j || j==0){
+                temp.push_back(1);
+            }
+            else{
+                temp.push_back(ans[i-1][j-1] + ans[i-1][j]);
+            }
+        }
+        ans.push_back(temp);
+    }
+    return ans;
+}
