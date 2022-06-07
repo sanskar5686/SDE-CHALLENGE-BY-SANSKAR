293ca35360024ad442d618dc1df90488bdba796f
sanskar5686 <88050679+sanskar5686@users.noreply.github.com>
2022-06-08 03:11:06 +0530

    Update DAY-1.cpp

--- DAY-1.cpp
+++ DAY-1.cpp
@@ -1,50 +1,16 @@
-#include <bits/stdc++.h> 
-void sort012(int *arr, int n)
-{
-   //   Write your code here
-      int nextZero=0;
-      int nextTwo = n-1;
-    for(int i=0,j=n-1;i<=nextTwo && j>0;){
-        if(arr[i] == 0){
-            swap(arr[i],arr[nextZero]);
-            i++;
-            nextZero++;
+#include<bits/stdc++.h>
+int maximumProfit(vector<int> &prices){
+    // Write your code here.
+    int n = prices.size(),least = INT_MAX;
+    int prev_diff = 0;
+    for(int i =0;i<n;i++){
+        if(prices[i] < least){
+            least = prices[i];
         }
-        else if(arr[i] == 2){
-             swap(arr[i],arr[nextTwo]);
-             nextTwo--;
-          }
-        else{
-            i++;
+        int diff = prices[i] - least;
+        if(diff > prev_diff){
+            prev_diff = diff;
         }
     }
-}
-
-2nd Approach
-
-void sort012(int *arr, int n){
-    int count0=0;
-    int count1=0;
-    int count2=0;
-    for(int i=0;i<n;i++){
-        if(arr[i] ==0){
-            count0++;
-        }
-        else if(arr[i] == 1){
-            count1++;
-        }
-        else if(arr[i] == 2){
-            count2++;
-        }
-    }
-    for(int i=0;i<count0;i++){
-        arr[i] = 0;
-    }
-     for(int i=count0;i<(count0+ count1);i++){
-        arr[i] = 1;
-    }
-     for(int i=count0+count1;i<n;i++){
-        arr[i] = 2;
-    }
-    
+    return prev_diff;
 }
