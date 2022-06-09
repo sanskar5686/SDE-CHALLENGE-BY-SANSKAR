205c6ef7848151fcdffed525d38879ec2f9161bf
sanskar5686 <88050679+sanskar5686@users.noreply.github.com>
2022-06-10 02:39:18 +0530

    Update DAY-3.cpp

--- DAY-3.cpp
+++ DAY-3.cpp
@@ -1,20 +1,27 @@
 // 1st approach 
-// Using hashmaps
-#include<bits/stdc++.h>
+// Using hashmaps - Here we have to iterate in the entire vector
 
-int findMajorityElement(int arr[], int n) {
-	// Write your code here.
+#include <bits/stdc++.h> 
+vector<int> majorityElementII(vector<int> &arr)
+{
+    // Write your code here.
+    int n = arr.size();
     unordered_map<int,int> map;
     for(int i=0;i<n;i++){
         map[arr[i]]++;
     }
-    int ans =-1;
+    set<int> ans_lite;
     for(int i=0;i<n;i++){
-        if(map[arr[i]] > n/2){
-            ans = arr[i];
+        if(map[arr[i]] > n/3){
+            ans_lite.insert(arr[i]);
         }
     }
-    return ans;
+    vector<int> ans;
+    set<int, greater<int> >::iterator itr;
+   for(itr=ans_lite.begin();itr!=ans_lite.end();itr++)
+    ans.push_back(*itr);
+    
+   return ans;
 }
 
 // 2nd approach 
