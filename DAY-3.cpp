eedc25d57a652448a4fa18a675137f92d13a9cbd
sanskar5686 <88050679+sanskar5686@users.noreply.github.com>
2022-06-10 02:38:15 +0530

    Update DAY-3.cpp

--- DAY-3.cpp
+++ DAY-3.cpp
@@ -1,1 +1,20 @@
+// 1st approach 
+// Using hashmaps
+#include<bits/stdc++.h>
 
+int findMajorityElement(int arr[], int n) {
+	// Write your code here.
+    unordered_map<int,int> map;
+    for(int i=0;i<n;i++){
+        map[arr[i]]++;
+    }
+    int ans =-1;
+    for(int i=0;i<n;i++){
+        if(map[arr[i]] > n/2){
+            ans = arr[i];
+        }
+    }
+    return ans;
+}
+
+// 2nd approach 
