d76d0315b373843111fa44a52f822e7272f5cf0d
sanskar5686 <88050679+sanskar5686@users.noreply.github.com>
2022-06-10 01:37:39 +0530

    Update DAY-2.cpp

--- DAY-2.cpp
+++ DAY-2.cpp
@@ -1,1 +1,36 @@
 
+// 1st approach 
+// Time complexity -> N(Log(N)) + O(N)
+
+#include <bits/stdc++.h> 
+int findDuplicate(vector<int> &arr, int n){
+	 // Write your code here.
+          sort(arr.begin(), arr.end());
+    int ans =0;
+    for(int i=0;i<n-1;i++){
+        if(arr[i] == arr[i+1]){
+            ans = arr[i];
+        }
+    }
+    return ans;
+}
+
+// 2nd approach 
+// Time complexity  -> O(N)
+// Space complexity -> O(N)
+
+#include <bits/stdc++.h> 
+int findDuplicate(vector<int> &arr, int n){
+	 // Write your code here.
+          int ans =0;
+    int freq[n+1] = {0};
+    for(int i=0;i<n;i++){
+       freq[arr[i]]++;
+    }
+    for(int i=0;i<n;i++){
+        if(freq[arr[i]] >= 2){
+            ans = arr[i];
+        }
+    }
+    return ans;
+}
