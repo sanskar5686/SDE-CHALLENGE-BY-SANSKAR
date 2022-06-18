787ebc22ce1c9cf370244d59f3ffdcafa3625b40
sanskar5686 <88050679+sanskar5686@users.noreply.github.com>
2022-06-18 14:36:20 +0530

    Update DAY-4.cpp

--- DAY-4.cpp
+++ DAY-4.cpp
@@ -1,1 +1,19 @@
-
+// Approach - 1 (Naive approach)
+// T.c -> O(N^2) 
+#include <bits/stdc++.h> 
+int uniqueSubstrings(string arr)
+{
+    //Write your code here
+    int maxans = -1;
+    for(int i=0;i<arr.size();i++){
+        unordered_set<int> set;
+        for(int j=i;j<arr.size();j++){
+          if(set.find(arr[j]) != set.end()){
+              maxans = max(maxans, j-i);
+              break;
+          }
+            set.insert(arr[j]);
+        }
+    }
+    return maxans;
+}
