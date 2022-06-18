448e448803a8d2785e333ab598d25b816ed1f3bd
sanskar5686 <88050679+sanskar5686@users.noreply.github.com>
2022-06-19 02:17:45 +0530

    Update DAY-4.cpp

--- DAY-4.cpp
+++ DAY-4.cpp
@@ -17,3 +17,55 @@ int uniqueSubstrings(string arr)
     }
     return maxans;
 }
+//Approach - 2 
+// Using O(2N) solution and space is O(N)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string arr) {
+        int n = arr.size();
+        // if(n==0){
+        //     return 0;
+        // }
+        set<int> s;
+        int left = 0, right =0;
+        int len =0;
+       // set<int> s;
+        while(right < n){
+           if(s.find(arr[right]) != s.end()){
+               while(left < right  && s.find(arr[right]) != s.end()){
+                   s.erase(arr[left]);
+                   left++;
+               }
+           }
+            s.insert(arr[right]);
+            len = max(len, right-left+1);
+            right++;
+        }
+        return len;
+    }
+};
+
+// Approach - 3 (Best approach)
+// Using O(N) solution and space is O(1)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string arr) {
+        int n = arr.size();
+        // if(n==0){
+        //     return 0;
+        // }
+        int left = 0, right =0;
+        int len =0;
+        vector<int> map(256,-1);
+        while(right < n){
+            if(map[arr[right]] != -1){
+                // update l
+                left= max(left,map[arr[right]]+1);
+            }
+            map[arr[right]] = right;
+            len = max(len, right-left+1);
+            right++;
+        }
+        return len;
+    }
+};
